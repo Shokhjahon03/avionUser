b45b6b16a496ff8d55ea9e70ce46de5526d4ff69
Shohjahon03 <quvvatovshohjahon707@gmail.com>
2024-03-19 20:21:18 +0500

    add

--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -1,9 +1,23 @@
+'use client'
+import allDatas from "@/store/store"
+import axios from "axios"
 import Link from "next/link"
+import { useEffect, useState } from "react"
 
 const Navbar = () => {
+  let [c,setC]=useState(0)
+  let [z,setZ]=useState(true)
+  let func=async()=>{
+      let r=await axios.get('https://65f258c5034bdbecc7649f36.mockapi.io/app/local')
+      let d=await r.data
+      setC(d.length)
+  }
+  useEffect(()=>{
+    func()
+  },[c])
   return (
     <>
-     <nav className="md:mb-[25px]">
+     <nav className="md:mb-[25px] relative">
        <div className="container md:pl-[20px] md:pr-[20px]">
         <div className="navbar w-full h-[69px] flex  items-center justify-between pl-[24px] pr-[20px] md:border-b">
           <button className="hidden md:block"><i className='bx bx-search'></i></button>
@@ -15,12 +29,22 @@ const Navbar = () => {
                 <Link href='/blog'>Blog</Link>
               </div>
            <button  className="md:hidden"><i className='bx bx-search md:hidden'></i></button>
-           <Link href='/cart' className="hidden md:block"><i className='bx bx-cart-alt'></i></Link>
-            <button  className="md:hidden"><i className='bx bx-menu' ></i></button>
+           <Link href='/cart' className="hidden md:block relative"><i className='bx bx-cart-alt'></i><p className="bg-[] z-20 text-white absolute top-[-7px] text-[10px] left-[-5px]">{c}</p><i className=' absolute top-[-10px] left-[-12px] text-[20px] z-10 text-blue-500 bx bxs-bell'  ></i></Link>
+            <button onClick={()=>setZ(!z)} className="md:hidden"><i className='bx bx-menu' ></i></button>
             <button className="hidden md:block"><i className='bx bx-user-circle'></i></button>
             </div>
         </div>
-        </div> 
+        </div>
+
+
+        <div className={z?'left md:hidden':'right md:hidden'}>
+          <Link href='/'>Home</Link>
+          <Link href='/aboute'>Aboute</Link>
+          <Link href='/blog'>Home</Link>
+          <Link href='/cart'>Cart</Link>
+          </div>
+
+
     </nav> 
     <div className="w-full">
     <div className="container">
